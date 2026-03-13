19423cea74b76f036deae666f76235d2427da17b
GwynnN7 <mattcheru03@gmail.com>
2026-03-13 12:00:22 +0100

    Fix permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.gwynn7.motolog"
         minSdk = 28
         targetSdk = 35
-        versionCode = 16
-        versionName = "2.0"
+        versionCode = 17
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
